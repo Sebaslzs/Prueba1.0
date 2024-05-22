41fb3d8a7fb23f1e02708265fc6937f386e92a8a
sebaslzs <sebastianlopezsanches46@gmail.com>
2024-05-22 15:51:41 -0500

    ejercicio 2

---
+++ ejercico 2/ejercico 2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new TipCalculatorModel());
+        }
+
+        [HttpPost]
+        public IActionResult CalculateTip(TipCalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", model);
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ ejercico 2/ejercico 2/Models/TipCalculatorModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        [Required(ErrorMessage = "El monto de la factura es obligatorio.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Por favor, ingrese un monto positivo.")]
+        public double BillAmount { get; set; }
+
+        [Required(ErrorMessage = "El porcentaje de propina es obligatorio.")]
+        [Range(0, 100, ErrorMessage = "Por favor, ingrese un porcentaje entre 0 y 100.")]
+        public double TipPercentage { get; set; }
+
+        public double TipAmount => BillAmount * (TipPercentage / 100);
+
+        public double TotalAmount => BillAmount + TipAmount;
+    }
+}
