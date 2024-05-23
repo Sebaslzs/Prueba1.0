773e50ace63893cddb2c0fbfa2d6de1661f01191
sebaslzs <sebastianlopezsanches46@gmail.com>
2024-05-23 14:15:41 -0500

    subir carpetas

---
+++ Cronometro online/Cronometro online/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cronometro_online.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
