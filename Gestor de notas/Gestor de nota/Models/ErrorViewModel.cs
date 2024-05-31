29bdb310fa37dc9e2b8cad4dce2b6ea9e29fb29b
sebaslzs <sebastianlopezsanches46@gmail.com>
2024-05-31 14:48:55 -0500

    limpieza2

--- Gestor de notas/Gestor de nota/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Gestor_de_nota.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
